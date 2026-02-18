d08d8063361c334f56755eb72860dce3a087def7
RJbeley <rjbeley05@gmail.com>
2026-02-19 07:22:48 +0800

    Initial commit for gaussian naieve bayes pipeline

--- MachineLearning/gaussian_naive_bayes.py
+++ MachineLearning/gaussian_naive_bayes.py
@@ -1,8 +1,36 @@
-import pandas as pd
+from sklearn.naive_bayes import GaussianNB
+from .Utils.data_evaluation import data_evaluation
 
-class AdaBoostPipeline():
-    __init__(self):
-    pass
+class GaussianNaiveBayesPipeline:
+    def __init__(self, X_train, X_test,
+                 y_train, y_test,
+                 args, parameters=None):
+        self.args = args
+        self.parameters = parameters
+        self.model = None
+        self.results = None
 
-    def _data_preparation
-    
+        # Store pre‑prepared splits directly
+        self.X_train = X_train
+        self.X_test  = X_test
+        self.y_train = y_train
+        self.y_test  = y_test
+
+    def build_model(self):
+        # Initialize Gaussian Naive Bayes with provided parameters
+        # Note: GaussianNB only accepts a few parameters (like priors, var_smoothing)
+        self.model = GaussianNB(**(self.parameters or {}))
+        return self
+
+    def train(self):
+        if self.model is None:
+            raise ValueError("Model not built. Call build_model() first.")
+        self.model.fit(self.X_train, self.y_train)
+        return self
+
+    def evaluation(self):
+        self.results = data_evaluation(self.model, self.X_test, self.y_test)
+        return self
+
+    def show_results(self):
+        return self.results
