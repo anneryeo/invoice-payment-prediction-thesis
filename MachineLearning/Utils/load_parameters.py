f160f76291a2ebeed1bf9dd9aaa1e7de5bda213d
RJbeley <rjbeley05@gmail.com>
2026-02-19 07:27:16 +0800

    Initial commit for loading parameters helper class

--- MachineLearning/Utils/load_parameters.py
+++ MachineLearning/Utils/load_parameters.py
@@ -1,4 +1,16 @@
-import pandas as pd
+import json
 
-def load_parameters(json_file_location):
-    pass
+class ParameterLoader:
+    def __init__(self, json_file_location):
+        self.json_file_location = json_file_location
+        self.parameters = self._load_parameters()
+
+    def _load_parameters(self):
+        with open(self.json_file_location, "r") as f:
+            return json.load(f)
+
+    def get_parameters(self, model_name):
+        """
+        Returns list of parameter dicts for the given model.
+        """
+        return self.parameters.get(model_name, [])
