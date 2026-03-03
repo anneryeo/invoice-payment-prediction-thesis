4c897871121b44d4f2ea4e94b04cda5518995c36
RJbeley <rjbeley05@gmail.com>
2026-03-03 09:03:12 +0800

    Moved the adjustment of survival time periods to a seperate utility function and implemented it to the data prepararation step

--- MachineLearning/Utils/data_preparation.py
+++ MachineLearning/Utils/data_preparation.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 from imblearn.over_sampling import SMOTE, BorderlineSMOTE
@@ -126,28 +125,6 @@ class DataPreparer:
         self.X_test[numeric_cols] = scaler.transform(self.X_test[numeric_cols])
 
         return self
-
-    def _adjust_payment_period(self, T, ε=1e-6):
-        """
-        Adjusts the payment period to avoid negative or zero values.
-
-        Parameters
-        ----------
-        T : array-like or float
-            The time period(s) to adjust.
-        epsilon : float, optional
-            A small constant added to avoid zero values (default is 1e-6).
-
-        Returns
-        -------
-        adjusted_T : ndarray or float
-            The adjusted time period(s).
-        earliest_pre_payment : ndarray or float
-            The portion representing pre-payments (non-positive values).
-        """
-        earliest_pre_payment = np.minimum(T, 0)
-        adjusted_T = T - earliest_pre_payment + ε
-        return adjusted_T
 
     def decode_labels(self, y_encoded):
         if self.label_encoder is None:
