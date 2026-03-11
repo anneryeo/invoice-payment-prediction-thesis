5ee1d63c8c732116e4ef98e0cf991c3d1e6b4361
RJbeley <rjbeley05@gmail.com>
2026-03-11 12:35:08 +0800

    fix: Added a language tag in the html so that the browser no longer asks to translate the website.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,28 @@
-# app/__init__.py
 from dash import Dash
 import dash_bootstrap_components as dbc
 
-# Create the single Dash instance here
 dash_app = Dash(__name__,
            external_stylesheets=[dbc.themes.BOOTSTRAP],
            suppress_callback_exceptions=True)
+
+dash_app.index_string = '''
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        {%metas%}
+        <title>Invoice Payment Prediction</title>
+        {%favicon%}
+        {%css%}
+    </head>
+    <body>
+        {%app_entry%}
+        <footer>
+            {%config%}
+            {%scripts%}
+            {%renderer%}
+        </footer>
+    </body>
+</html>
+'''
 
 server = dash_app.server
